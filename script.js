b5e0b2c706b0e9fb98f590119c4cd0785dfd7672
MichaelTanaka <michaelotanaka@gmail.com>
2024-02-05 14:14:24 -0800

    steps 1-4 done input has id of 'text-input' button has id of 'check-btn' div has id of 'result' #check-btn alert user input value cannot be blank

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function palindrome(str) {
 }
 
 console.log(palindrome("A man, a plan, a canal. Panama"));
+
+const button = document.getElementById("check-btn");
+const input = document.getElementById("text-input");
+function handleInputCheck(e) {
+  e.preventDefault();
+  if (input.value == "") {
+    alert("Please input a value.");
+  }
+}
+
+button.addEventListener("click", handleInputCheck);
