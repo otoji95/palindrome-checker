1a484ccc87fb9a6f32ccebee0900bc03a793c400
MichaelTanaka <michaelotanaka@gmail.com>
2024-02-06 11:46:12 -0800

    completes isPalindrome function, fixes updateInputCheck to meet requirements

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function palindrome(str) {
+function isPalindrome(str) {
+  if (str.length == 1) return true;
+
+  let matches = str
+    .match(/[A-Za-z0-9]+/g)
+    .join("")
+    .toLowerCase();
+  for (let i = 0, j = matches.length - 1; i < j; i++, j--) {
+    if (matches[i] !== matches[j]) {
+      return false;
+    }
+  }
   return true;
 }
 
-console.log(palindrome("A man, a plan, a canal. Panama"));
-
 const button = document.getElementById("check-btn");
 const input = document.getElementById("text-input");
+const result = document.getElementById("result");
+const para = document.createElement("p");
+result.appendChild(para);
+
 function handleInputCheck(e) {
   e.preventDefault();
   if (input.value == "") {
     alert("Please input a value.");
   }
+
+  let answer = palindrome(input.value);
+  para.innerText = answer ? `${input.value} is a palindrome.` : `${input.value} is not a palindrome.`;
 }
 
 button.addEventListener("click", handleInputCheck);
